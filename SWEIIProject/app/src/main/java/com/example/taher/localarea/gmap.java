a2d4e715da9e20c8a64ee69b440e0fdc40c545ae
Kareem Salah <kimo.h2010@gmail.com>
2016-05-13 14:30:43 +0200

    Fixed issue #10

--- SWEIIProject/app/src/main/java/com/example/taher/localarea/gmap.java
+++ SWEIIProject/app/src/main/java/com/example/taher/localarea/gmap.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.GoogleMap;
